5800d9de341d42772d66f51a887ccacbc0119172
Luke Lowrey <luke.lowrey@achiive.co.uk>
2018-01-15 13:02:17 +0000

    Added Maze solver. Currently have two mazes in.

--- ktane.js
+++ ktane.js
@@ -6,9 +6,12 @@ angular.module('ktane', [])
 
         // We have an array of commonly misheard words for numbers here.
         var numbers = {
+            '0': 0,
             'zero': 0,
             'none': 0,
+            '1': 1,
             'one': 1,
+            '2': 2,
             'two': 2,
             'too': 2,
             'to': 2,
@@ -36,7 +39,8 @@ angular.module('ktane', [])
             batteries: {
                 value: null,
                 question: "How many batteries are there",
-                response: "(zero|none|one|two|too|to|three|four|for|five|six)"
+                response: "(zero|none|one|two|too|to|three|four|for|five|six)",
+                remap: numbers,
             },
             frk: {value: null, question: "Is there an indicator saying F R K", response: "(yes|no)"},
             car: {value: null, question: "Is there an indicator saying F R K", response: "(yes|no)"},
@@ -56,7 +60,11 @@ angular.module('ktane', [])
 
                 var assignProperty = function (value) {
                     $scope.annyang.removeCommands("property");
-                    property.value = value;
+                    if(property.remap){
+                        property.value = property.remap[value];
+                    }else {
+                        property.value = value;
+                    }
                     resolve(property.value);
                 };
 
@@ -180,16 +188,17 @@ angular.module('ktane', [])
                 }
 
                 if (params.numberOfWires === 5 && arguments.length === 5) {
-
+                    // TODO: 5 wires
                 }
 
                 if (params.numberOfWires === 6 && arguments.length === 6) {
-
+                    // TODO: 6 wires
                 }
             };
 
             var finish = function () {
                 $scope.annyang.removeCommands("simplewires:colours");
+                $scope.currentmodule = null;
             };
 
 
@@ -204,35 +213,323 @@ angular.module('ktane', [])
             return params;
         }();
 
-        $scope.buttons = {
-            name: "buttons",
-            init: function () {
+
+        $scope.button = function () {
+            var name = "button";
+            var params = {
+                colour: null,
+                word: null,
+            };
+
+            var button = function (colour, word) {
+                $scope.log("Button. Colour: " + colour + " word: " + word);
+                $scope.currentmodule = name;
+                params.colour = colour;
+                params.word = word;
+
+                if(word === "detonate"){
+                    $scope.property("batteries").then(function (batterycount) {
+                        if (batterycount > 1) {
+                            pressandrelease();
+                        } else {
+                            holdandchecklight();
+                        }
+                    });
+                }
+                if(colour === "white"){
+                    $scope.property("car").then(function (car) {
+                        if (car === "yes") {
+                            holdandchecklight();
+                        } else {
+                            checkfrk();
+                        }
+                    });
+                }
+
+                if(colour === "red" && word === "hold"){
+                    pressandrelease();
+                }
+
+                if(colour === "blue" && word === "abort"){
+                    holdandchecklight();
+                }
+
+                checkfrk();
+
+            };
+
+            var pressandrelease = function(){
+                $scope.say("Press and release the button");
+                finish();
+            };
+
+
+            var holdandchecklight = function() {
+                $scope.say("Press and hold the button");
+
                 $scope.annyang.addCommands({
-                    "buttons": {
-                        'regexp': /^button (?=.*\b(red|blue|white|yellow|black)\b)(?=.*\b(abort|hold|press|detonate)\b).*$/,
-                        'callback': this.buttons
+                    "button:heldcolour": {
+                        'regexp': /^(red|blue|yellow|white)?$/,
+                        'callback': heldcolour
                     }
                 });
 
-                return this;
-            },
-            buttons: function (colour, word) {
-                $scope.log("Button. Colour: " + colour + " word: " + word);
-                $scope.currentmodule = this.name;
+            };
+
+            var heldcolour = function(colour){
+                params.heldcolour = colour;
+                if(colour === "blue"){
+                    $scope.say("Release when timer has 4");
+                }else if(colour === "yellow"){
+                    $scope.say("Release when timer has 5");
+                }else{
+                    $scope.say("Release when timer has 1");
+                }
+                finish();
+            };
+
+            var checkfrk = function(){
+                $scope.property("batteries").then(function (batterycount) {
+                    if (batterycount > 2) {
+                        $scope.property("frk").then(function (frk) {
+                            if (frk === "yes") {
+                                pressandrelease();
+                            } else {
+                                holdandchecklight();
+                            }
+                        });
+
+                    } else {
+                        holdandchecklight();
+                    }
+                });
+            };
 
 
+            var finish = function () {
+                $scope.annyang.removeCommands("button:heldcolour");
+                $scope.currentmodule = null;
+            };
 
-                // $scope.annyang.addCommands({"buttons:colours" : {'regexp' : /^(red|blue|black|yellow|white) (red|blue|black|yellow|white) (red|blue|black|yellow|white) ?(red|blue|black|yellow|white)? ?(red|blue|black|yellow|white)? ?(red|blue|black|yellow|white)?$/ , 'callback': simplewires.wireColours}});
 
-            },
-            wireColours: function (c1, c2, c3, c4, c5, c6) {
-                $scope.log("Colours: " + c1 + c2 + c3 + c4 + c5 + c6);
-            },
+            $scope.annyang.addCommands({
+                "button": {
+                    'regexp': /^button (?=.*\b(red|blue|white|yellow|black)\b)(?=.*\b(abort|hold|press|detonate)\b).*$/,
+                    'callback': button
+                }
+            });
 
-            finish: function () {
-                // $scope.annyang.removeCommands("buttons:colours");
-            }
-        }.init();
+
+            return params;
+
+        }();
+
+        $scope.maze = function () {
+            var name = "maze";
+            var params = {
+                startingposition: [null, null],
+                targetposition: [null, null],
+            };
+
+            // top = 1
+            // left = 2
+            // bottom = 4
+            // right = 8
+
+            // topleft = 3
+
+            var selectedmaze = null;
+
+            // Converts the 'top/left' only values into a top/left/bottom/right value
+            var tl2tlbr = function(maze){
+                var i, j;
+
+                for(i = 0; i < 6; i++){
+                    for(j = 0; j < 6; j++){
+                        if(i === 5 || (maze[i + 1][j] & 1)){ // If we're at the bottom, or if the next row down has the 'top' bit set
+                            maze[i][j] |= 4; // then set the 'bottom' bit on this row
+                        }
+                    }
+                }
+
+                for(j = 0; j < 6; j++){
+                    for(i = 0; i < 6; i++){
+                        if(j === 5 || (maze[i][j+1] & 2)){ // If we're at the right, or if the next column across has the 'left' bit set
+                            maze[i][j] |= 8; // then set the 'bottom' bit on this row
+                        }
+                    }
+                }
+
+                return maze;
+
+            };
+
+            var mazes = {
+                a: tl2tlbr([ // 1,5 6,4
+                    [3, 1, 1, 3, 1, 1],
+                    [2, 3, 0, 2, 1, 1],
+                    [2, 2, 1, 3, 1, 0],
+                    [2, 3, 0, 0, 3, 0],
+                    [2, 1, 1, 3, 1, 2],
+                    [2, 1, 2, 0, 3, 0],
+                ]),
+                b: tl2tlbr([ // 2,3 5,5
+                    [3, 1, 1, 3, 1, 1],
+                    [3, 0, 3, 0, 2, 1],
+                    [2, 3, 0, 3, 1, 0],
+                    [2, 0, 3, 0, 3, 2],
+                    [2, 3, 2, 3, 0, 2],
+                    [2, 2, 0, 2, 1, 0],
+                ]),
+            };
+
+            var mazemarkers = {
+                12: mazes.a,
+                63: mazes.a,
+                24: mazes.b,
+                52: mazes.b,
+            };
+
+            var maze = function (x, y) {
+
+                console.log(mazes);
+                $scope.log("Maze");
+                $scope.currentmodule = name;
+
+                x = numbers[x];
+                y = numbers[y];
+
+                selectedmaze = mazemarkers[(x * 10) + y];
+
+                $scope.say("Starting position");
+
+                $scope.annyang.addCommands({
+                    "maze:startingposition": {
+                        'regexp': /^(one|two|too|to|three|four|for|1|2|4|3|5|6|five|six) (one|two|too|to|three|four|for|1|2|4|3|5|6|five|six)$/,
+                        'callback': currentposition
+                    }
+                });
+
+            };
+
+            var currentposition = function(x, y){
+                x = numbers[x];
+                y = numbers[y];
+
+                params.startingposition = [x - 1, y - 1];
+
+                $scope.annyang.removeCommands("maze:startingposition");
+                $scope.say("Target");
+
+                $scope.annyang.addCommands({
+                    "maze:target": {
+                        'regexp': /^(one|two|too|to|three|four|for|1|2|4|3|5|6|five|six) (one|two|too|to|three|four|for|1|2|4|3|5|6|five|six)$/,
+                        'callback': target
+                    }
+                });
+
+
+            };
+
+
+            var visited = null;
+
+            var target = function(x, y){
+
+                $scope.annyang.removeCommands("maze:target");
+
+                x = numbers[x];
+                y = numbers[y];
+
+                params.targetposition = [x - 1, y - 1];
+
+                // Now we have to solve it! Woot.
+
+                // Log which cells have been visited (so that we don't backtrack)
+                visited = [
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0],
+                ];
+
+
+                var location = {
+                    _x: params.startingposition[0],
+                    _y: params.startingposition[1],
+                    path: [],
+                };
+
+
+                var queue = [location];
+
+                var directions = [
+                    {label: 'up', dy: -1, dx: 0, bit: 1},
+                    {label: 'left', dy: 0, dx: -1, bit: 2},
+                    {label: 'down', dy: 1, dx: 0, bit: 4},
+                    {label: 'right', dy: 0, dx: 1, bit: 8},
+                ];
+
+                while (queue.length > 0) {
+                    // Take the first location off the queue
+                    var currentLocation = queue.shift();
+
+                    var _y = currentLocation._y;
+                    var _x = currentLocation._x;
+
+
+                    if(params.targetposition[0] === _x && params.targetposition[1] === _y){
+                        location = currentLocation;
+                        break;
+                    }
+
+                    visited[_y][_x] = 1;
+
+
+                    _.each(directions, function(direction){
+                        var newPath = currentLocation.path.slice();
+                        newPath.push(direction.label); // Textual description of this current path.
+
+                        // If we can transition in the direction we want to go, and we've not visited that cell, add it to the queue
+                        if((selectedmaze[_y][_x] & direction.bit) === 0 && visited[_y + direction.dy][_x + direction.dx] === 0) {
+                            queue.push( {
+                                _x: _x + direction.dx,
+                                _y: _y + direction.dy,
+                                path: newPath,
+                            });
+                        }
+
+
+                    });
+
+
+                } // end while
+
+
+                $scope.say(currentLocation.path.join(" "));
+
+                finish();
+            };
+
+
+            var finish = function () {
+                $scope.currentmodule = null;
+            };
+
+
+            $scope.annyang.addCommands({
+                "maze": {
+                    'regexp': /^maze (one|two|too|to|three|four|for|1|2|4|3|5|6|five|six) (one|two|too|to|three|four|for|1|2|4|3|5|6|five|six)$/,
+                    'callback': maze
+                }
+            });
+
+
+            return params;
+
+        }();
 
 
         // Start listening. You can call this here, or attach this call to an event, button, etc.
